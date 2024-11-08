b41e233416e1852630fda11342e87d98a79aca7e
wlfn <szkolapawel16@gmail.com>
2024-11-08 08:37:10 +0100

    Change setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,6 @@
 cd /D "%~dp0"
-if not exists .venv\(
-    py -m venv .venv
-)
+if not exists .\.venv\ py -m venv .venv
+
 .venv\Scripts\activate.bat && py -m pip install pytesseract pillow numpy keyboard pyautogui
 echo "A website will open. Download the newest release and install into this directory with the addition of `/tesseract` in the path"
 pause
